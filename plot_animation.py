af4bfb1855839e8fc7b8e941d87d248d8815c34d
DJimmie <dowdjimmie10@gmail.com>
2018-10-07 13:13:03 -0500

    Attempts to get the Data_Sim_1 to call the plot_animation.py. Some code changes associated with this effort. Not sucessfull. (See issue #1)

--- plot_animation.py
+++ plot_animation.py
@@ -8,34 +8,41 @@ import os
 style.use(style='fivethirtyeight')
 
 
+def the_animator():
+
+    
+    def animate(r):
+        graph_data=open(os.getcwd()+'\\real_time_data.txt','r').read()
+        lines=graph_data.split('\n')
+    ##    print (lines)
+        xs=[]
+        ys=[]
+        for line in lines:
+            if len(line)>1:
+                x,y=line.split(',')
+                xs.append(float(x))
+                ys.append(float(y))
+
+    ##            print(float(x),float(y))
+                
+    ##    ax1.set_ylim(-1,1)
+        ax1.set_xlim(0,600)  
+        ax1.plot(xs,ys, linewidth=1,color='b')
+        ax2.hist(ys,bins=50,color='blue')
+##        ax2.hist(abs(ys),color='blue')
 
 
-fig=plt.figure()
-ax1=fig.add_subplot(2,1,1)
-ax2=fig.add_subplot(2,1,2)
-##ax1.clear()
+    
+    fig=plt.figure()
+    ax1=fig.add_subplot(2,1,1)
+    ax2=fig.add_subplot(2,1,2)
+    ##ax1.clear()
+    ax1.clear()
+    ax2.clear()
+    ani=animation.FuncAnimation(fig,animate,interval=1000)
+    plt.show()
 
-def animate(r):
-    graph_data=open(r'C:\Users\Crystal\Desktop\Programs\live_plots\real_time_data.txt','r').read()
-    lines=graph_data.split('\n')
-##    print (lines)
-    xs=[]
-    ys=[]
-    for line in lines:
-        if len(line)>1:
-            x,y=line.split(',')
-            xs.append(float(x))
-            ys.append(float(y))
-
-##            print(float(x),float(y))
-            
-##    ax1.set_ylim(-1,1)
-    ax1.set_xlim(0,600)  
-    ax1.plot(xs,ys, linewidth=1,color='b')
-##    ax2.hist(ys,bins=50,color='blue')
-    ax2.acorr(abs(ys),color='blue')
-
-ax1.clear()
-ax2.clear()
-ani=animation.FuncAnimation(fig,animate,interval=1000)
-plt.show()
+if __name__ == '__main__':
+    
+    the_animator()
+    print ('im done')
